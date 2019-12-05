c0a473572b6eada693322bbf4aa27c09b66ec1bf
vjosey <53488086+vjosey@users.noreply.github.com>
2019-12-05 18:11:48 -0500

    $         modified:   src/app/app-routing.module.ts bash: modified:: command not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
