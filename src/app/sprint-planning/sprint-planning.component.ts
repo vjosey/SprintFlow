522edd003fb9b164b78a6aa4d00089b262b48ffa
vjosey <53488086+vjosey@users.noreply.github.com>
2019-12-06 06:34:26 -0500

    modified:   angular.json         modified:   src/app/app-routing.module.ts         modified:   src/app/app.component.html         modified:   src/app/app.component.ts         modified:   src/app/app.module.ts         modified:   src/app/sprint-planning/sprint-planning.component.css         modified:   src/app/sprint-planning/sprint-planning.component.html         modified:   src/app/sprint-planning/sprint-planning.component.ts         modified:   src/index.html

--- src/app/sprint-planning/sprint-planning.component.ts
+++ src/app/sprint-planning/sprint-planning.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-sprint-planning',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SprintPlanningComponent implements OnInit {
 
-  constructor() { }
+
+stories: UserStory[] = USERSTORE;
+
+selectedStory: UserStory;
+
+userStory: UserStory = {
+  id: 1,
+  title: 'New',
+  detail: 'new stuff'
+};
+
+constructor() {
+
+}
 
   ngOnInit() {
   }
 
+selectedUserStory(story: UserStory) {
+ this.selectedStory = story;
+ $('StoryModal').modal('show');
 }
+
+spOn() {
+  // alert( 'hello' );
+  $('#newStoryModal').modal('show');
+
+}
+
+saveNewUserStory() {
+
+  // add to array
+  this.userStory.id = 16; // can to auto id
+  this.stories.push(this.userStory);
+  $('#newStoryModal').modal('hide');
+  this.userStory = null;
+}
+
+
+
+}
+
+export class UserStory {
+  id: number;
+  title: string;
+  detail: string;
+
+  constructor(){}
+
+}
+
+export const USERSTORE: UserStory[] = [
+  { id: 11, title: 'Dr Nice' , detail: 'dbkfjnkejnfdskn'},
+  { id: 12, title: 'Narco' , detail: 'dbkfjnkejnfdskn'},
+  { id: 13, title: 'Bombasto' , detail: 'dbkfjnkejnfdskn'},
+  { id: 14, title: 'Celeritas', detail: 'dbkfjnkejnfdskn' },
+];
